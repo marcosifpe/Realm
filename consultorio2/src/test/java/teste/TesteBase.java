fd553a707ef8cd93e32ef2ceed73ff3b4e123bae
marcosifpe <marcosifpe@gmail.com>
2018-10-20 09:33:25 -0300

    Versao inicial

---
+++ consultorio2/src/test/java/teste/TesteBase.java
@@ -0,0 +1,84 @@
+package teste;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+/**
+ *
+ * @author valeria
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SuppressWarnings("JPQLValidation")
+public class TesteBase {
+
+    private static EntityManagerFactory emf;
+    public static Logger logger = Logger.getGlobal();
+    public EntityManager em;
+    private EntityTransaction et;
+
+    public TesteBase() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        //logger.setLevel(Level.INFO);
+        logger.setLevel(Level.SEVERE);
+        emf = Persistence.createEntityManagerFactory("consultorio");
+        DbUnitUtil.inserirDados();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        emf.close();
+    }
+
+    @Before
+    public void setUp() {
+        em = emf.createEntityManager();
+        beginTransaction();
+    }
+
+    @After
+    public void tearDown() {
+        commitTransaction();
+        em.close();
+    }
+
+    private void beginTransaction() {
+        et = em.getTransaction();
+        et.begin();
+    }
+
+    private void commitTransaction() {
+        try {
+            et.commit();
+        } catch (Exception ex) {
+            //logger.log(Level.SEVERE, ex.getMessage(), ex);
+            if (et.isActive()) {
+                et.rollback();
+            }
+            //fail(ex.getMessage());
+        }
+    }
+
+    public Date getData(Integer dia, Integer mes, Integer ano) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, ano);
+        c.set(Calendar.MONTH, mes);
+        c.set(Calendar.DAY_OF_MONTH, dia);
+        return c.getTime();
+    }
+}
